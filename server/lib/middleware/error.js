216d2683f34025029bad05e6769961f07591e98f
dalhun namgung <bnamgung@gmail.com>
2018-02-19 13:18:06 -0800

    working on

---
+++ server/lib/middleware/error.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+
+module.exports = (err, req, res, next) => {
+    console.log("ERR", err);
+
+    if( err.statusCode ) {
+        return res.sendStatus(err.statusCode);
+    }
+    return res.sendStatus(500);
+};
